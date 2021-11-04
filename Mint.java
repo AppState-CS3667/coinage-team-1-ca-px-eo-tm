60d771f578ca2f59b2a30b587a27b26b040ee3d9
Peter Xiong <peter92xiong@gmail.com>
2021-11-03 20:41:37 -0400

    added the mint.java class, not finish, still need some work

---
+++ Mint.java
@@ -0,0 +1,22 @@
+/**
+ * this is the mint method it should only have 1 instance of it (a singleton)
+ * 
+ * @author Team 1
+ *
+ */
+public class Mint {
+
+    /**
+     * 
+     * making coin. Factory method USDmint CADmint EURmint GBPmint will implement
+     * makeCoin method. only 1 mint. A siglenton
+     * 
+     * @return a coin object
+     */
+    public Coin makeCoin() {
+
+        return null;
+
+    }
+
+}
