f514e7c27ea71b6562fa118b9014df76cc141351
Tristan Mosher <Tmmmosher@gmail.com>
2021-11-10 10:29:09 -0500

    added euromint to demo

--- Demo.java
+++ Demo.java
@@ -1,6 +1,9 @@
 public class Demo{
     public static void main(String[] args){
         USDMint m = USDMint.getInstance();
+
+        EURMint e = EURMint.getInstance();
+
         m.makeCoin("Penny");
         System.out.println("");
         m.makeCoin("Nickel");
@@ -12,5 +15,22 @@ public class Demo{
         m.makeCoin("HalfDollar");
         System.out.println("");
         m.makeCoin("Dollar");
+
+        e.makeCoin("OneEuro");
+        System.out.println("");
+        e.makeCoin("TwoEuro");
+        System.out.println("");
+        e.makeCoin("FiftyEuroCent");
+        System.out.println("");
+        e.makeCoin("TwentyEuroCent");
+        System.out.println("");
+        e.makeCoin("TenEuroCent");
+        System.out.println("");
+        e.makeCoin("FiveEuroCent");
+        System.out.println("");
+        e.makeCoin("TwoEuroCent");
+        System.out.println("");
+        e.makeCoin("EuroCent");
+
     }
 }
